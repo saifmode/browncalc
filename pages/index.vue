b877fa48e6433db24ec3309d8473c77cfdcc7a7f
Ben Hunter <ben.hunter@zoodigital.com>
2021-04-28 22:54:45 +0100

    No wrap title

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          <div style="width: 100%; text-align: center; font-size: 15px;">
+          <div style="width: 100%; text-align: center; white-space: nowrap; font-size: 21px;">
               Brown Reclaimed Shelving Unit Price Calculator
           </div>
         </v-card-title>
