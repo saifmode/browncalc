2dcf36a951a4efb84cc093dae27d27be3fb20736
Ben Hunter <ben.hunter@zoodigital.com>
2021-05-06 21:55:13 +0100

    Delete unneeded imports

--- pages/index.vue
+++ pages/index.vue
@@ -25,18 +25,6 @@
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
-</script>
-
 <style>
 .d-flex {
   display: flex;
